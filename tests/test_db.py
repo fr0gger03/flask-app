3d20c5ac79f4fc6e77910508f9bca56d2b7ae548
Tom Twyman <thomas.twyman@docker.com>
2024-08-20 14:30:14 -0400

    updated compose for watch functionality
    Signed-off-by: Tom Twyman <thomas.twyman@docker.com>

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,5 +1,9 @@
 import io
 import pytest
+import sqlalchemy
+
+from pathlib import Path
+from testcontainers.postgres import PostgresContainer
 from src.app import app
 
 @pytest.fixture
@@ -7,6 +11,68 @@ def client():
     app.testing = True
     with app.test_client() as client:
         yield client
+
+
+def test_docker_run_postgres_with_sqlalchemy():
+    postgres_container = PostgresContainer("postgres:16.3-bookworm")
+    with postgres_container as postgres:
+        engine = sqlalchemy.create_engine(postgres.get_connection_url())
+        with engine.begin() as connection:
+            result = connection.execute(sqlalchemy.text("select version()"))
+            for row in result:
+                assert row[0].lower().startswith("postgresql 16.3")
+
+
+# This is a feature in the generic DbContainer class
+# but it can't be tested on its own
+# so is tested in various database modules
+def test_quoted_password():
+    user = "postgres"
+    password = "postgres"
+    quoted_password = "postgres"
+    driver = "psycopg2"
+    port = "5432"
+
+    kwargs = {
+        "driver": driver,
+        "user": user,
+        "password": password,
+        "port":port
+    }
+    with PostgresContainer("postgres:16.3", **kwargs) as container:
+        port = container.get_exposed_port(5432)
+        host = container.get_container_host_ip()
+        expected_url = f"postgresql+{driver}://{user}:{quoted_password}@{host}:{port}/test"
+
+        url = container.get_connection_url()
+        assert url == expected_url
+
+        with sqlalchemy.create_engine(expected_url).begin() as connection:
+            connection.execute(sqlalchemy.text("select 1=1"))
+
+        raw_pass_url = f"postgresql+{driver}://{user}:{password}@{host}:{port}/test"
+        with pytest.raises(Exception):
+            # it raises ValueError, but auth (OperationalError) = more interesting
+            with sqlalchemy.create_engine(raw_pass_url).begin() as connection:
+                connection.execute(sqlalchemy.text("select 1=1"))
+
+
+def test_show_how_to_initialize_db_via_initdb_dir():
+    postgres_container = PostgresContainer("postgres:16.3-bookworm")
+    script = Path(__file__).parent / "src" / "sql" / "init-user-db.sh"
+    postgres_container.with_volume_mapping(host=str(script), container=f"/docker-entrypoint-initdb.d/{script.name}")
+
+    insert_query = "insert into example(name, description) VALUES ('sally', 'sells seashells');"
+    select_query = "select id, name, description from example;"
+
+    with postgres_container as postgres:
+        engine = sqlalchemy.create_engine(postgres.get_connection_url())
+        with engine.begin() as connection:
+            connection.execute(sqlalchemy.text(insert_query))
+            result = connection.execute(sqlalchemy.text(select_query))
+            result = result.fetchall()
+            assert len(result) == 1
+            assert result[0] == (1, "sally", "sells seashells")
 
 
 def test_login_post(client):
